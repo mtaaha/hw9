fd3b9fd3ed3f58eb96551f8edb98874e75818325
mtaaha <taahamohammad@gmail.com>
2018-12-10 20:34:39 -0500

    Uploaded files

---
+++ js/jQuery.js
@@ -0,0 +1,39 @@
+/*
+   Name: Mohammad Taaha
+   91.61 Assignment: Using the jQuery UI Slider and Tab Widgets
+   Mohammad Taaha, UMass Lowell Computer Science Student,mohammad_taaha@student.uml.edu
+   Copyright (c) 2018 by Mohammad Taaha.  All rights reserved.  May be
+freely copied or excerpted for educational purposes with credit to the
+author.
+   Created on 12/04/2018
+	 Description:This webpage will ask the user to enter th einput to create
+   a multiplication table. It will then create that table in a new tab. The user will
+   be able to select the values of the multiplication table using a slider. In this
+   assignment a slider and tabs were implemented. When the slider is moved, it adjusts
+   the value in the text box.
+*/
+$(function () {
+		 $( "#scrabble_board_1" ).draggable();
+		 $( "#scrabble_board_2" ).draggable();
+		 $( "#scrabble_board_3" ).draggable();
+		 $( "#scrabble_board_4" ).draggable();
+		 $( "#scrabble_board_5" ).draggable();
+		 $( "#scrabble_board_6" ).draggable();
+		 $( "#scrabble_board_7" ).draggable();
+		 $( "#scrabble_board_8" ).draggable();
+		 $( "#scrabble_board_9" ).draggable();
+		 $( "#scrabble_board_10" ).draggable();
+		 $( "#scrabble_board_11" ).draggable();
+		 $( "#scrabble_board_12" ).draggable();
+		 $( "#droppable" ).droppable({
+	drop: function( event, ui ) {
+		$( this )
+			.addClass( "ui-state-highlight" )
+			.find( "p" )
+				.html( "Dropped!" );
+	}
+});
+$.getJSON("pieces.json", function( data ) {
+			console.log(data);
+		});
+});
